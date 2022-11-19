7896d8308a5488a12bff9196e7ce4b7fd34330a4
mado <madelineadonley@gmail.com>
2022-11-19 10:42:15 -0700

    added enviornment variables

--- music_share/spotify/util.py
+++ music_share/spotify/util.py
@@ -1,9 +1,15 @@
 from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
-from .credentials import CLIENT_ID, CLIENT_SECRET
 from requests import post, put, get
 
+# get enviornment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
 BASE_URL = "https://api.spotify.com/v1/me/"
 
